3c25ffdf15a63ce037ce658bf27f99b1bd1a458b
Khanh Chung <khanh.d.chung@gmail.com>
2025-03-31 00:01:16 -0700

    add try catch block to the Main

--- proj2/gitlet/Main.java
+++ proj2/gitlet/Main.java
@@ -10,36 +10,40 @@ public class Main {
     /** Usage: java gitlet.Main ARGS, where ARGS contains
      *  <COMMAND> <OPERAND1> <OPERAND2> ...
      */
-    public static void main(String[] args) throws IOException {
-        if (args.length == 0) {
-            Utils.message("Please enter a command.");
-            System.exit(0);
-        }
-
-        String firstArg = args[0];
-        switch(firstArg) {
-            case "init":
-                Repository.init();
-                break;
-            case "add":
-                Repository.add(args[1]);
-                break;
-            case "commit":
-                Repository.commit(args[1]);
-                break;
-            case "checkout":
-                if (args.length == 3) {
-                    Repository.checkout(args[2]);
-                } else {
-                    Repository.checkout(args[1], args[3]);
-                }
-                break;
-            case "log":
-                Repository.log();
-                break;
-            default:
-                Utils.message("No command with that name exists.");
+    public static void main(String[] args) {
+        try {
+            if (args.length == 0) {
+                Utils.message("Please enter a command.");
                 System.exit(0);
+            }
+
+            String firstArg = args[0];
+            switch (firstArg) {
+                case "init":
+                    Repository.init();
+                    break;
+                case "add":
+                    Repository.add(args[1]);
+                    break;
+                case "commit":
+                    Repository.commit(args[1]);
+                    break;
+                case "checkout":
+                    if (args.length == 3) {
+                        Repository.checkout(args[2]);
+                    } else {
+                        Repository.checkout(args[1], args[3]);
+                    }
+                    break;
+                case "log":
+                    Repository.log();
+                    break;
+                default:
+                    Utils.message("No command with that name exists.");
+                    System.exit(0);
+            }
+        } catch (IOException e) {
+            Utils.error(e.toString());
         }
     }
 }
